a3e7c6fe12c88825ca5eead3f12419e3a7b0aca4
aladales <diagenin@gmail.com>
2023-09-01 20:39:45 +0300

    added 00 and 01 day 01 not finished

--- day01/ex04/Program.java
+++ day01/ex04/Program.java
@@ -7,7 +7,6 @@ public class Program {
 
         TransactionsService transactionsService = new TransactionsService();
 
-        // Создание и добавление пользователей
         User user1 = new User("John Doe", 100.0);
         User user2 = new User("Jane Smith", 200.0);
 
@@ -15,33 +14,39 @@ public class Program {
         transactionsService.addUser(user2);
         try {
 
-            // Проверка баланса пользователя
             double balance1 = transactionsService.getUserBalance(0);
-            System.out.println("User 1 balance: " + balance1); // Ожидаемый результат: 100.0
+            double balance2 = transactionsService.getUserBalance(1);
 
-            // Выполнение транзакции
+            System.out.println("User 1 balance before transaction: " + balance1);
+            System.out.println("User 2 balance before transaction: " + balance2);
+
             transactionsService.performTransaction(0, 1, balance1);
 
-            // Проверка баланса после транзакции
-            double balance2 = transactionsService.getUserBalance(0);
-            double balance3 = transactionsService.getUserBalance(1);
-            System.out.println("User 1 balance after transaction: " + balance2); // Ожидаемый результат: 50.0
-            System.out.println("User 2 balance after transaction: " + balance3); // Ожидаемый результат: 250.0
+            double balance3 = transactionsService.getUserBalance(0);
+            double balance4 = transactionsService.getUserBalance(1);
+            System.out.println("User 1 balance after transaction: " + balance3);
+            System.out.println("User 2 balance after transaction: " + balance4);
 
-            // Получение списка транзакций пользователя
             TransactionsList user1Transactions = transactionsService.getUserTransactions(0);
-            System.out.println("User 1 transactions: " + user1Transactions);
+            System.out.println("User 1 transactions: " + user1Transactions.toArray().length);
 
-            // Удаление транзакции
+            TransactionsList user2Transactions = transactionsService.getUserTransactions(1);
+            System.out.println("User 2 transactions: " + user2Transactions.toArray().length);
+
             transactionsService.removeTransaction(1, 1);
 
-            // Получение списка непарных транзакций
+            user1Transactions = transactionsService.getUserTransactions(0);
+            System.out.println("User 1 transactions: " + user1Transactions.toArray().length);
+
+            user2Transactions = transactionsService.getUserTransactions(1);
+            System.out.println("User 2 transactions after remove transaction: " + user2Transactions.toArray().length);
+
             ArrayList<Transaction> unpairedTransactions = transactionsService.getUnpairedTransactions();
 
             System.out.println("Unpaired transactions: " + unpairedTransactions.size());
 
         } catch (Exception e) {
-            System.out.println("TransactionNotFoundException");
+            System.out.println(e);
         }
     }
 }
